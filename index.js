5ac77fef5405300b76fd1b005d3b314d86f100a1
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-04-06 09:55:29 +0530

    Commit

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-require('dotenv').config()
+// require('dotenv').config()
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 app.use(express.json)
 app.use(cors())
-const PORT = 8080
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}...`)
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`Server is running on PORT ${port}...`)
 })
 
 const mongoose = require('mongoose')
 
-const DB = prosess.env.MongoDB
+const DB = process.env.LINK
 mongoose.connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
